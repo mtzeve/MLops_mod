9afbce1e9f4f12a1c159a5653e5f4c134483dfda
Manos Tzevelekos <114588453+mtzeve@users.noreply.github.com>
2024-05-14 17:27:32 +0200

    Update run_inference_pipeline.sh

--- Stocks_news_prediction/Notebooks/run_inference_pipeline.sh
+++ Stocks_news_prediction/Notebooks/run_inference_pipeline.sh
@@ -3,5 +3,7 @@
 set -e
 
 cd /Stocks_news_prediction/Notebooks
+chmod +x ./Stocks_news_prediction/Notebooks/run_inference_pipeline.sh
+
 
 jupyter nbconvert --to notebook --execute 8_inference_pipeline.ipynb
