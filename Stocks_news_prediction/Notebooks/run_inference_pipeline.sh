fac219625cdb18f31183403b3f16f294099a78c5
mtzeve <manolistzevelekos@gmail.com>
2024-05-14 16:52:57 +0200

    updated_17

--- Stocks_news_prediction/Notebooks/run_inference_pipeline.sh
+++ Stocks_news_prediction/Notebooks/run_inference_pipeline.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cd /Users/manos/Documents/BDS/MLops_mod/Stocks_news_prediction/Notebooks
+cd /Stocks_news_prediction/Notebooks
 
 jupyter nbconvert --to notebook --execute 8_inference_pipeline.ipynb
