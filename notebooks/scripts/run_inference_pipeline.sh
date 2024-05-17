47f5e17b9e7a3eb0f603848cd3cb7485f0db79ad
FrederikKl <149702620+FrederikKl@users.noreply.github.com>
2024-05-17 10:14:44 +0200

    Update run_inference_pipeline.sh

--- notebooks/scripts/run_inference_pipeline.sh
+++ notebooks/scripts/run_inference_pipeline.sh
@@ -4,6 +4,18 @@ set -e
 
 cd ./notebooks/SML
 
-#python feature_pipeline.py
+python historical_news.py
+
+python historical_stock.py
+
+python news_preprocessing.py
+
+python stock_preprocessing.py
+
+python feature_pipeline.py
+
+python feature_view.py
+
+python training_pipeline.py
 
 python inference_pipeline.py
